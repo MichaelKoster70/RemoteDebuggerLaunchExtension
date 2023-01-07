521eecdcada0692f1c4f18da73fb0f070fa5edad
Michael Koster <michael.koster@bluewin.ch>
2023-01-07 16:33:08 +0100

    fixed analyzer warnings

--- samples/AspNetSpa/Program.cs
+++ samples/AspNetSpa/Program.cs
@@ -1,36 +1,43 @@
+// ----------------------------------------------------------------------------
+// <copyright company="Michael Koster">
+//   Copyright (c) Michael Koster. All rights reserved.
+//   Licensed under the MIT License.
+// </copyright>
+// ----------------------------------------------------------------------------
+
 namespace AspNetSpa
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            var builder = WebApplication.CreateBuilder(args);
+   public static class Program
+   {
+      public static void Main(string[] args)
+      {
+         var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
+         // Add services to the container.
 
-            builder.Services.AddControllersWithViews();
+         _ = builder.Services.AddControllersWithViews();
 
-            var app = builder.Build();
+         var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
-            if (!app.Environment.IsDevelopment())
-            {
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
+         // Configure the HTTP request pipeline.
+         if (!app.Environment.IsDevelopment())
+         {
+            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+            _ = app.UseHsts();
+         }
 
-            //app.UseHttpsRedirection();
-            app.UseStaticFiles();
-            app.UseRouting();
+         //app.UseHttpsRedirection();
+         _ = app.UseStaticFiles();
+         _ = app.UseRouting();
 
 
-            app.MapControllerRoute(
+         _ = app.MapControllerRoute(
                 name: "default",
                 pattern: "{controller}/{action=Index}/{id?}");
 
-            app.MapFallbackToFile("index.html");
+         _ = app.MapFallbackToFile("index.html");
 
-            app.Run();
-        }
-    }
+         app.Run();
+      }
+   }
 }
