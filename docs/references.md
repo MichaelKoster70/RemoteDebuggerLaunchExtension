119ea29bb25ed0e8ef4d11118429fa7686badf3f
Michael Koster <michael.koster@bluewin.ch>
2023-01-19 21:56:59 +0100

    added dedicated image for the launch profile

--- docs/references.md
+++ docs/references.md
@@ -3,6 +3,12 @@
 ## Publish a VSIX Extension to the Marketplace
 [Walkthrough: Publishing a Visual Studio extension via command line](https://learn.microsoft.com/en-us/visualstudio/extensibility/walkthrough-publishing-a-visual-studio-extension-via-command-line?view=vs-2022)
 
+## Visual Studio Project System
+
+[Extenibility Docummentation: microsoft/VSProjectSystem](https://github.com/microsoft/VSProjectSystem)
+[Source: dotnet/project-system](https://github.com/dotnet/project-system)
+
+
 ## Analyiers
 
 [Microsoft.CodeAnalysis.Analyzers](https://github.com/dotnet/roslyn-analyzers/blob/main/src/Microsoft.CodeAnalysis.Analyzers/Microsoft.CodeAnalysis.Analyzers.md)
---
+++ src/Extension/RemoteDebuggerLauncher/ImagesMonikers.cs
@@ -0,0 +1,33 @@
+﻿// ----------------------------------------------------------------------------
+// <copyright company="Michael Koster">
+//   Copyright (c) Michael Koster. All rights reserved.
+//   Licensed under the MIT License.
+// </copyright>
+// ----------------------------------------------------------------------------
+
+using System;
+using Microsoft.VisualStudio.Imaging.Interop;
+
+namespace RemoteDebuggerLauncher
+{
+   /// <summary>
+   /// Utility class holding the <see cref="ImageMoniker"/> for the images provided by the package.
+   /// </summary>
+   public static class ImagesMonikers
+   {
+      private static readonly Guid manifestGuid = new Guid("C7D7D241-8FD3-4230-8F49-314652792839");
+      private const int LaunchProfileIcon = 0;
+      private const int DeployOutputIcon = 1;
+      private const int CleanOutputIcon = 2;
+      private const int InstallDebuggerIcon = 3;
+      private const int InstallDotnetIcon = 4;
+      private const int SetupSshIcon = 5;
+
+      public static ImageMoniker LaunchProfileIconImageMoniker => new ImageMoniker { Guid = manifestGuid, Id = LaunchProfileIcon };
+      public static ImageMoniker DeployOutputIconImageMoniker => new ImageMoniker { Guid = manifestGuid, Id = DeployOutputIcon };
+      public static ImageMoniker CleanOutputIconImageMoniker => new ImageMoniker { Guid = manifestGuid, Id = CleanOutputIcon };
+      public static ImageMoniker InstallDebuggerIconImageMoniker => new ImageMoniker { Guid = manifestGuid, Id = InstallDebuggerIcon };
+      public static ImageMoniker InstallDotnetIconImageMoniker => new ImageMoniker { Guid = manifestGuid, Id = InstallDotnetIcon };
+      public static ImageMoniker SetupSshIconImageMoniker => new ImageMoniker { Guid = manifestGuid, Id = SetupSshIcon };
+   }
+}
