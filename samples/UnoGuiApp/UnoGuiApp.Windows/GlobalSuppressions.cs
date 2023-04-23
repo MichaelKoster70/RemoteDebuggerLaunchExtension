83a49d73e0cc99eafae21d07eef6765fc896b9d6
Michael Koster <MichaelKoster70@users.noreply.github.com>
2023-04-23 16:44:58 +0200

    Updated samples

---
+++ samples/UnoGuiApp/UnoGuiApp.Windows/GlobalSuppressions.cs
@@ -0,0 +1,10 @@
+﻿// ----------------------------------------------------------------------------
+// <copyright company="Michael Koster">
+//   Copyright (c) Michael Koster. All rights reserved.
+//   Licensed under the MIT License.
+// </copyright>
+// ----------------------------------------------------------------------------
+
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("Security", "CA5392:Use DefaultDllImportSearchPaths attribute for P/Invokes", Justification = "Generated code", Scope = "member", Target = "~M:UnoGuiApp.Program.XamlCheckProcessRequirements")]
