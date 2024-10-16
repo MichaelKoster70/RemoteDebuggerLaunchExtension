3ec65cbab6a0785dd0eab387652cf70c6573e0fc
Michael Koster <MichaelKoster70@users.noreply.github.com>
2024-10-16 11:20:21 +0200

    fix build issues (#54)
    * remove cache
    * upgrade to Angular 15
    * fix warnings

--- samples/net6/WebBlazorWasmApp/Server/Controllers/WeatherForecastController.cs
+++ samples/net6/WebBlazorWasmApp/Server/Controllers/WeatherForecastController.cs
@@ -19,11 +19,11 @@ namespace WebBlazorWasmApp.Server.Controllers
         "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
     };
 
-      private readonly ILogger<WeatherForecastController> _logger;
+      private readonly ILogger<WeatherForecastController> logger;
 
       public WeatherForecastController(ILogger<WeatherForecastController> logger)
       {
-         _logger = logger;
+         this.logger = logger;
       }
 
       [HttpGet]
