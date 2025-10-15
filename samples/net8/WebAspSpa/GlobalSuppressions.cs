0a2824d08ba8539f30bcc73b60a244f2a1e7314b
Michael Koster <michael.koster@agi-sda.com>
2025-10-15 17:37:03 +0200

    fix dependabot issues

--- samples/net8/WebAspSpa/GlobalSuppressions.cs
+++ samples/net8/WebAspSpa/GlobalSuppressions.cs
@@ -8,3 +8,4 @@
 using System.Diagnostics.CodeAnalysis;
 
 [assembly: SuppressMessage("Security", "CA5394:Do not use insecure randomness", Justification = "Just for demo purposes", Scope = "member", Target = "~M:AspNetSpa.Controllers.WeatherForecastController.Get~System.Collections.Generic.IEnumerable{AspNetSpa.WeatherForecast}")]
+[assembly: SuppressMessage("Maintainability", "CA1515:Consider making public types internal", Justification = "Just for demo purposes", Scope = "type", Target = "~T:AspNetSpa.Controllers.WeatherForecastController")]
