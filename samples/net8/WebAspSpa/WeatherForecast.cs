25fae17ef56915991e6028b484518d5deafed778
Michael Koster <michael.koster@agi-sda.com>
2025-10-16 09:55:47 +0200

    update to .NET 8.0.21

--- samples/net8/WebAspSpa/WeatherForecast.cs
+++ samples/net8/WebAspSpa/WeatherForecast.cs
@@ -5,16 +5,15 @@
 // </copyright>
 // ----------------------------------------------------------------------------
 
-namespace AspNetSpa
-{
-    public class WeatherForecast
-    {
-        public DateOnly Date { get; set; }
+namespace WebAspSpa;
 
-        public int TemperatureC { get; set; }
+ public class WeatherForecast
+ {
+     public DateOnly Date { get; set; }
 
-        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+     public int TemperatureC { get; set; }
 
-        public string? Summary { get; set; }
-    }
-}
+     public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+     public string? Summary { get; set; }
+ }
