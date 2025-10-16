88d6640babc0a4f70c3aa309ae72a0dd336199c8
Michael Koster <michael.koster@agi-sda.com>
2025-10-16 10:49:36 +0200

    updated angular app

--- samples/net8/WebAspSpa/ClientApp/src/app/counter/counter.component.ts
+++ samples/net8/WebAspSpa/ClientApp/src/app/counter/counter.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-counter-component',
-  templateUrl: './counter.component.html'
+  templateUrl: './counter.component.html',
+  standalone: false
 })
 export class CounterComponent {
   public currentCount = 0;
