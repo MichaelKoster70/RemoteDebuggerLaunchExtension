17a7ec95fb00be0225ea0e30630aedc947981d45
Michael Koster <michael.koster@agi-sda.com>
2025-10-16 13:32:07 +0200

    Update Angular to 19

--- samples/net9/WebAspSpa/ClientApp/src/app/nav-menu/nav-menu.component.ts
+++ samples/net9/WebAspSpa/ClientApp/src/app/nav-menu/nav-menu.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-nav-menu',
   templateUrl: './nav-menu.component.html',
+  standalone: false,
   styleUrls: ['./nav-menu.component.css']
 })
 export class NavMenuComponent {
