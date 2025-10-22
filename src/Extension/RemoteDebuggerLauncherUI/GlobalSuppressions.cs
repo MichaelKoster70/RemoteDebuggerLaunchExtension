ae2219c1e4b52b5696670f13aad9211ff5418654
Michael Koster <michael.koster@agi-sda.com>
2025-10-22 17:39:07 +0200

    Fix the UI

---
+++ src/Extension/RemoteDebuggerLauncherUI/GlobalSuppressions.cs
@@ -0,0 +1,14 @@
+﻿// ----------------------------------------------------------------------------
+// <copyright company="Michael Koster">
+//   Copyright (c) Michael Koster. All rights reserved.
+//   Licensed under the MIT License.
+// </copyright>
+// ----------------------------------------------------------------------------
+
+// This file is used by Code Analysis to maintain SuppressMessage attributes that are applied to this project.
+// Project-level suppressions either have no target or are given a specific target and scoped to a namespace, type, member, etc.
+
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("Minor Code Smell", "S2325:Methods and properties that don't access instance data should be static", Justification = "By design", Scope = "member", Target = "~M:RemoteDebuggerLauncher.RemoteOperations.SecureShellPassphraseViewModel.HandleOkCommand(Microsoft.VisualStudio.PlatformUI.DialogWindow)")]
+[assembly: SuppressMessage("Minor Code Smell", "S2325:Methods and properties that don't access instance data should be static", Justification = "By design", Scope = "member", Target = "~M:RemoteDebuggerLauncher.RemoteOperations.SecureShellPassphraseViewModel.HandleCancelCommand(Microsoft.VisualStudio.PlatformUI.DialogWindow)")]
---
+++ src/Extension/RemoteDebuggerLauncherUI/RemoteOperations/Views/SecureShellPassphraseDialogWindow.xaml.cs
@@ -0,0 +1,28 @@
+﻿// ----------------------------------------------------------------------------
+// <copyright company="Michael Koster">
+//   Copyright (c) Michael Koster. All rights reserved.
+//   Licensed under the MIT License.
+// </copyright>
+// ----------------------------------------------------------------------------
+
+using Microsoft.VisualStudio.PlatformUI;
+
+namespace RemoteDebuggerLauncher.RemoteOperations
+{
+   /// <summary>
+   /// Modal Dialog providing the UI options for the Install .NET Command.
+   /// Implements the <see cref="DialogWindow" />
+   /// </summary>
+   /// <seealso cref="DialogWindow" />
+   public partial class SecureShellPassphraseDialogWindow : DialogWindow
+   {
+      /// <summary>
+      /// Initializes a new instance of the <see cref="SecureShellPassphraseDialogWindow"/> class.
+      /// </summary>
+      public SecureShellPassphraseDialogWindow()
+      {
+         InitializeComponent();
+      }
+   }
+}
+
