5024681f39ee0ce49c8a2bf8520a78d5030b9e26
Michael Koster <MichaelKoster70@users.noreply.github.com>
2025-12-28 17:55:38 +0100

    Upgrade samples (#89)
    * Upgrade .NET 8&9 samples
    * Upgrade UNO to v6.2 for .NET 8
    * Upgrade UNO to v6.4 for .NET 9

---
+++ samples/net8/UnoGuiApp/UnoGuiApp/Platforms/Desktop/Program.cs
@@ -0,0 +1,28 @@
+// ----------------------------------------------------------------------------
+// <copyright company="Michael Koster">
+//   Copyright (c) Michael Koster. All rights reserved.
+//   Licensed under the MIT License.
+// </copyright>
+// ----------------------------------------------------------------------------
+
+using Uno.UI.Hosting;
+
+namespace UnoGuiApp;
+
+internal static class Program
+{
+    [STAThread]
+    public static void Main(string[] args)
+    {
+
+        var host = UnoPlatformHostBuilder.Create()
+            .App(() => new App())
+            .UseX11()
+            .UseLinuxFrameBuffer()
+            .UseMacOS()
+            .UseWin32()
+            .Build();
+
+        host.Run();
+    }
+}
